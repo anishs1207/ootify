82a80d84291109e8f22aca002f5b008438d7ed9e
Anish <anishs1207@gmail.com>
2025-08-22 16:41:11 +0530

    merged firebase in page.tsx itself

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 "use client";
 
-import { auth, provider, signInWithPopup } from "../app/firebase";
 import { BlurText, TrueFocus } from "@/components/dashboard";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
 import Header from "@/components/landing/Header";
 import Footer from "@/components/landing/Footer";
 
 export default function Home() {
     const router = useRouter();
+
+    const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    let analytics;
+    if (typeof window !== "undefined") {
+        analytics = getAnalytics(app);
+    }
+
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
 
     const handleAnimationComplete = () => {
         console.log("Animation completed!");
